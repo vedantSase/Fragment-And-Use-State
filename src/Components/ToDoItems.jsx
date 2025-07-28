92cc76c7d76eb9d8630c96863962aa966656dc75
vedantSase <sasevedant5264@gmail.com>
2025-07-28 23:19:31 +0530

    Use of useContext

--- src/Components/ToDoItems.jsx
+++ src/Components/ToDoItems.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { ToDoTasks_Context } from "../Store/ToDoTasks-Context.jsx";
 import TDItem from "./TDItem";
-function ToDoItems({ tasks, deleteTask }) {
+function ToDoItems() {
+    // Using useContext to get the tasks from the context
+    const { tasks, deleteTask } = useContext(ToDoTasks_Context);
+    // console.log(ToDOTasksFromContext);
     return (
         <div className="items-container ">
-            {tasks.map((TDtask) => (<TDItem key={TDtask.task} task={TDtask.task} date={TDtask.date} deleteTask={deleteTask}></TDItem>
+            {tasks.map((TDtask) => (<TDItem key={TDtask.task} task={TDtask.task} date={TDtask.date} ></TDItem>
             ))}
         </div>
     );
