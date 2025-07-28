5f1c4baedc3316baf5668773a7a9ead418e24384
vedantSase <sasevedant5264@gmail.com>
2025-07-28 23:14:54 +0530

    verifying length in its own

--- src/Components/NoTask.jsx
+++ src/Components/NoTask.jsx
@@ -1,8 +1,11 @@
 import './../App.css';
+import { useContext } from 'react';
+import { ToDoTasks_Context } from '../Store/ToDoTasks-Context.jsx';
 function NoTask() {
-    return (
-        <h4 className="NoTask">Enjoy Your Day...</h4>
-    );
+    const { tasks } = useContext(ToDoTasks_Context);
+
+
+    return tasks.length === 0 && <h4 className="NoTask">Enjoy Your Day...</h4>
 }
 
 export default NoTask;
